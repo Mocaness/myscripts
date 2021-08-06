16f9e277a657fb89993c0e1787759bd9ca03bcbd
Mocarafee <harif.rp05@gmail.com>
2021-08-06 16:54:22 +0700

    scripts: revert zipname to default

--- gkgcc.sh
+++ gkgcc.sh
@@ -155,10 +155,10 @@ gen_zip() {
 	msg "|| Zipping into a flashable zip ||"
 	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
 	cd AnyKernel3 || exit
-	zip -r9 $ZIPNAME-$DEVICE-"$CI_BRANCH"-"$DRONE_BUILD_NUMBER" ./* -x .git README.md
+	zip -r9 $ZIPNAME-$DEVICE-"$DRONE_BUILD_NUMBER" ./* -x .git README.md
 
 	## Prepare a final zip variable
-	ZIP_FINAL="$ZIPNAME-$DEVICE-$CI_BRANCH-$DRONE_BUILD_NUMBER.zip"
+	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
 	tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 	cd ..
 }
