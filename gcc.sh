7871f61b79bb1bcfdd9565df5221b3e72ad1df79
Harif R. P <harif.rp05@gmail.com>
2022-07-08 05:13:01 +0700

    Update gcc script

--- gcc.sh
+++ gcc.sh
@@ -35,45 +35,35 @@ err() {
 # The defult directory where the kernel should be placed
 KERNEL_DIR=$PWD
 
-# The name of the Kernel, to name the ZIP
+# Devices variable
 ZIPNAME="KalinaNegev"
-
-# The codename of the device
 DEVICE="whyred"
-
-# The defconfig which should be used. Get it from config.gz from
-# your device or check source
 DEFCONFIG=whyred_defconfig
 
-##------------------------------------------------------##
-##---------Do Not Touch Anything Beyond This------------##
+# EnvSetup
+KBUILD_BUILD_USER="Mocarafee"
+KBUILD_BUILD_HOST="BlacksmithHouse"
+export CHATID="-1001262484455"
+export KBUILD_BUILD_HOST KBUILD_BUILD_USER
 
-## Set defaults first
-CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-token=$TELEGRAM_TOKEN
-export KBUILD_BUILD_HOST CI_BRANCH
-
-## Export CI Env
 export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
 export CI_BRANCH=$DRONE_BRANCH
-export CHATID="-1001262484455"
 
-#Check Kernel Version
+# Check Kernel Version
 KERVER=$(make kernelversion)
-
 
 # Set a commit head
 COMMIT_HEAD=$(git log --oneline -1)
 
-#Now Its time for other stuffs like cloning, exporting, etc
+##-----------------------------------------------------##
 
- clone() {
+clone() {
 	echo " "
-		msg "|| Cloning GCC ||"
-		git clone --depth=1 https://github.com/Mocaness/aarch64-elf-gcc -b 10.2 gcc64
-		git clone --depth=1 https://github.com/Mocaness/arm-eabi-gcc -b 10.2 gcc32
-		GCC64_DIR=$KERNEL_DIR/gcc64
-		GCC32_DIR=$KERNEL_DIR/gcc32
+	msg "|| Cloning GCC ||"
+	git clone --depth=1 https://github.com/Mocaness/aarch64-elf-gcc -b 10.2 gcc64
+	git clone --depth=1 https://github.com/Mocaness/arm-eabi-gcc -b 10.2 gcc32
+	GCC64_DIR=$KERNEL_DIR/gcc64
+	GCC32_DIR=$KERNEL_DIR/gcc32
 
 	msg "|| Cloning Anykernel ||"
 	git clone --depth 1 --no-single-branch https://github.com/Mocaness/AnyKernel3.git -b main
@@ -82,17 +72,16 @@ COMMIT_HEAD=$(git log --oneline -1)
 ##------------------------------------------------------##
 
 exports() {
-	export KBUILD_BUILD_USER="Mocarafee"
-	export KBUILD_BUILD_HOST="HaachamaKitchen"
 	export ARCH=arm64
 	export SUBARCH=arm64
+	export token=$TELEGRAM_TOKEN
 
-	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
-	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
-
-	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
+	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64*-elf-gcc --version | head -n 1)
+	PATH=$GCC64_DIR/bin/:/usr/bin:$PATH
 	export CROSS_COMPILE=$GCC64_DIR/bin/aarch64-elf-
+	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
 	export PATH KBUILD_COMPILER_STRING
+
 	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
 	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
 	PROCS=$(nproc --all)
@@ -112,7 +101,6 @@ tg_post_msg() {
 ##----------------------------------------------------------------##
 
 tg_post_build() {
-	#Show the Checksum alongwith caption
 	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
 	-F chat_id="$2"  \
 	-F "disable_web_page_preview=true" \
@@ -125,19 +113,18 @@ tg_post_build() {
 build_kernel() {
 
 	tg_post_msg "<b>🔨 $KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>" "$CHATID"
+	make O=out $DEFCONFIG
 
 	msg "|| Started Compilation ||"
 	BUILD_START=$(date +"%s")
-	make O=out $DEFCONFIG
-	make -j"$PROCS" O=out #LD=ld.lld
-
-		BUILD_END=$(date +"%s")
-		DIFF=$((BUILD_END - BUILD_START))
+	make -j"$PROCS" O=out
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
 
 		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
 	    then
 	    	msg "|| Kernel successfully compiled ||"
-				gen_zip
+			gen_zip
 		else
 		tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
 		fi
@@ -156,6 +143,7 @@ gen_zip() {
 	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
 	tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 	cd ..
+	rm -rf AnyKernel3
 }
 
 clone
